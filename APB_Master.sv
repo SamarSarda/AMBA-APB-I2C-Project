dbaa0069e3869bd3322a5f1dfed7a7339cf497fb
Justcallmeivan <ivan@DESKTOP-4C34VEL.localdomain>
2021-05-05 07:35:40 -0400

    apb takes 2 slaves now, tested

--- APB_Master.sv
+++ APB_Master.sv
@@ -21,13 +21,19 @@
 
 
 //needs error code to account for processor providing invalid input combinations e.g. start = 1, but sel = 0
-module APB_Master(APB_Bus.master ms, Processor_Bus.master pm);
+module APB_Master(APB_Bus.master ms1, APB_Bus.master ms2, Processor_Bus.master pm);
     logic [2:0] state;
     logic [2:0] next_state;
     parameter s_idle = 0, s_setup = 1, s_access = 2;
     logic after_ready;
     
-    assign ms.clk = pm.clk;
+    logic write, ready, enable, reset;
+    logic [7:0] wdata, rdata, addr, wait_cycles;
+    logic [1:0] sel;
+    
+    
+    assign ms1.clk = pm.clk;
+    assign ms2.clk = pm.clk;
     //assign pm.ready = ms.ready;
     //assign pm.rdata = ms.rdata;
     //States
@@ -42,7 +48,7 @@ module APB_Master(APB_Bus.master ms, Processor_Bus.master pm);
         end else if (state == s_setup) begin
             next_state = s_access; //access always happens on next clock after setup phase
         end else if (state == s_access) begin
-            case (ms.ready)
+            case ((ms1.ready && sel == 1) || (ms2.ready && sel == 2))
                 1'b0:
                     begin
                         next_state = s_access;
@@ -77,9 +83,26 @@ module APB_Master(APB_Bus.master ms, Processor_Bus.master pm);
     //Control Signals
     always @(posedge pm.clk) begin 
         if (state == s_idle) begin
-            ms.sel <= 2'b00;
-            ms.enable <= 1'b0;
-            pm.rdata <= ms.rdata;
+            if (sel == 1) begin // last select input
+                ms1.sel <= 2'b00;
+                ms1.enable <= 1'b0;
+                pm.rdata <= ms1.rdata;
+                
+                ready <= ms1.ready;
+            end else if (sel == 2) begin
+                ms2.sel <= 2'b00;
+                ms2.enable <= 1'b0;
+                pm.rdata <= ms2.rdata;
+                
+                ready <= ms2.ready;
+            end else begin
+                //not error, just idles
+            end
+            
+                sel <= 2'b00;
+                enable <= 1'b0;
+                rdata <= ms2.rdata;
+            
             if (after_ready) begin
                 pm.stable <= 1'b1;
                 after_ready <= 0;
@@ -87,19 +110,56 @@ module APB_Master(APB_Bus.master ms, Processor_Bus.master pm);
         end else if (state == s_setup) begin
             //should be only place where address, wdata and wait cycles are changed, 
             //by arm documentation, not changing addr or wdata unless there is a new transfer saves power
-            ms.sel <= pm.sel; // assume that pm will always give valid id, but consider throwing errors
-            ms.enable <= 1'b0;
-            ms.write <= pm.write;
-            ms.addr <= pm.addr;
-            ms.wdata <= pm.wdata;
-            pm.rdata <= ms.rdata;
+            if (pm.sel == 1) begin
+                ms1.sel <= pm.sel; // assume that pm will always give valid id, but consider throwing errors
+                ms1.enable <= 1'b0;
+                ms1.write <= pm.write;
+                ms1.addr <= pm.addr;
+                ms1.wdata <= pm.wdata;
+                pm.rdata <= ms1.rdata;
+                
+                ready <= ms1.ready;
+            end else if (pm.sel == 2) begin
+                ms2.sel <= pm.sel; // assume that pm will always give valid id, but consider throwing errors
+                ms2.enable <= 1'b0;
+                ms2.write <= pm.write;
+                ms2.addr <= pm.addr;
+                ms2.wdata <= pm.wdata;
+                pm.rdata <= ms2.rdata;
+                
+                ready <= ms2.ready;
+            end else begin
+                //error
+            end
+                sel <= pm.sel; // assume that pm will always give valid id, but consider throwing errors
+                enable <= 1'b0;
+                write <= pm.write;
+                addr <= pm.addr;
+                wdata <= pm.wdata;
+                rdata <= ms1.rdata;
+                
+            
             if (after_ready) begin
                 pm.stable <= 1'b1;
                 after_ready <= 0;
             end
         end else if (state == s_access) begin
-            ms.sel <= pm.sel; // assume that pm will always give valid id, but consider throwing errors
-            ms.enable <= 1'b1;
+            if (pm.sel == 1) begin
+                ms1.sel <= pm.sel; 
+                ms1.enable <= 1'b1;
+                
+                ready <= ms1.ready;
+            end else if (pm.sel == 2) begin
+                ms2.sel <= pm.sel;
+                ms2.enable <= 1'b1;
+                
+                ready <= ms2.ready;
+            end else begin
+                //error
+            end
+            sel <= pm.sel;
+            enable <= 1'b1;
+            
             pm.stable <= 1'b0;
             after_ready <= 1;
         end
--- apb_master_tb.sv
+++ apb_master_tb.sv
@@ -36,20 +36,21 @@ module apb_master_tb();
     logic [7:0] a_wdata, a_rdata, a_addr;
     logic [1:0] a_sel;
     
-    APB_Bus APB_i(clk);
+    APB_Bus APB_1(clk);
+    APB_Bus APB_2(clk);
 
     Processor_Bus Processor_bus_i(clk, reset);
-    //APB_Slave dut(.sl(APB_i.slave), .msl(Memory_Bus_i.slave));
-    APB_Master dut(APB_i.master, Processor_bus_i.master);
+    //APB_Slave dut(.sl(APB_1.slave), .msl(Memory_Bus_i.slave));
+    APB_Master dut(APB_1.master, APB_2.master, Processor_bus_i.master);
     
-    assign APB_i.ready = a_ready;
-    assign enable = APB_i.enable;
-    assign a_write = APB_i.write;
-    assign a_reset = APB_i.reset;
-    assign a_wdata = APB_i.wdata;
-    assign a_addr = APB_i.addr;
-    assign a_sel = APB_i.sel;
-    assign APB_i.rdata = a_rdata;
+    assign APB_1.ready = a_ready;
+    assign enable = APB_1.enable;
+    assign a_write = APB_1.write;
+    assign a_reset = APB_1.reset;
+    assign a_wdata = APB_1.wdata;
+    assign a_addr = APB_1.addr;
+    assign a_sel = APB_1.sel;
+    assign APB_1.rdata = a_rdata;
     
     
     assign Processor_bus_i.write = p_write;
@@ -102,7 +103,7 @@ module apb_master_tb();
         p_sel = 0;
         @(posedge clk);
         @(posedge clk);     //write transfer with wait states
-        //APB_i.wait_cycles = 5;
+        //APB_1.wait_cycles = 5;
         @(posedge clk);
         p_write = 1;
         p_sel = 1;
@@ -123,7 +124,7 @@ module apb_master_tb();
         p_sel = 0;  
         @(posedge clk);
         @(posedge clk);     //read transfer with wait states
-        //APB_i.wait_cycles = 5;
+        //APB_1.wait_cycles = 5;
         @(posedge clk);
         p_write = 0;
         p_sel = 1;
@@ -144,7 +145,7 @@ module apb_master_tb();
         p_sel = 0;  
         @(posedge clk);
         @(posedge clk);     //write transfer with wait states
-        //APB_i.wait_cycles = 1;
+        //APB_1.wait_cycles = 1;
         @(posedge clk);
         p_write = 1;
         p_sel = 1;
@@ -161,7 +162,7 @@ module apb_master_tb();
         p_sel = 0;  
         @(posedge clk);
         @(posedge clk);     //read transfer with wait states
-        //APB_i.wait_cycles = 1;
+        //APB_1.wait_cycles = 1;
         @(posedge clk);
         p_write = 0;
         p_sel = 1;
